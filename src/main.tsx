f01c25db27953aa01c1ad459da65bba26b40d619
Thế Đạt <74842495+tdat1310@users.noreply.github.com>
2025-01-04 16:27:41 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'https://tdat1310.github.io/product-crud/src/index.css'
+import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
